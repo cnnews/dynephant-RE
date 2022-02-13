f67cb0c28af1b084b970bf2ef678cc9bcb4a6ace
cnnews <46521067+cnnews@users.noreply.github.com>
2022-02-13 17:11:47 +0800

    Update update_dynv6.net.bat

--- update_dynv6.net.bat
+++ update_dynv6.net.bat
@@ -1,1 +1,1 @@
-D:\dynephant-master\dynephant -6 -daemon=300 -host=cnnews.v6.rocks -token=randomtextforfoobarhere 1>D:\dynephant-master\dynephant.log 2>&1
+D:\dynephant-RE-main\dynephant -6 -daemon=300 -host=cnnews.v6.rocks -token=randomtextforfoobarhere 1>D:\dynephant-master\dynephant.log 2>&1
